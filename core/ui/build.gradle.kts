6aa04c7805801421c3aef8a830a5fea60c1be6bb
StopStone <wltjr6432@naver.com>
2025-05-03 02:15:41 +0900

    Feat: core:ui 모듈 구성
    - app 모듈에 기본으로 생성된 theme을 core:ui로 이동

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("android-module")
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    namespace = "com.cyberwarriers.zubzub.core.ui"
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // 컴포즈 라이브러리
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    debugImplementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+}
