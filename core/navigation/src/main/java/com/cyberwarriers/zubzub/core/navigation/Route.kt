b1c73d51fcb9aa209fbe5e8152d932dd376c031a
StopStone <wltjr6432@naver.com>
2025-05-03 02:16:16 +0900

    Feat: core:navigation 모듈 구성
    - 딥링크 Route 구성

---
+++ core/navigation/src/main/java/com/cyberwarriers/zubzub/core/navigation/Route.kt
@@ -0,0 +1,13 @@
+package com.cyberwarriers.zubzub.core.navigation
+
+object Route {
+    // 스플래시 관련 경로
+    object Splash {
+        const val ROUTE = "splash"
+    }
+
+    // 인증 관련 경로
+    object Auth {
+        const val LOGIN = "login"
+    }
+}
