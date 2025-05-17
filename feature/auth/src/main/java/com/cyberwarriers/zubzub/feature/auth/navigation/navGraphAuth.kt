167b4303edb80df32f693115217a880aad231c74
StopStone <wltjr6432@naver.com>
2025-05-18 00:57:50 +0900

    Feat: 로그인 성공 시 메인 화면으로 이동

--- feature/auth/src/main/java/com/cyberwarriers/zubzub/feature/auth/navigation/navGraphAuth.kt
+++ feature/auth/src/main/java/com/cyberwarriers/zubzub/feature/auth/navigation/navGraphAuth.kt
@@ -11,11 +11,10 @@ fun NavGraphBuilder.navGraphAuth(navController: NavController) {
     composable(route = Route.Login) {
         LoginScreen(
             onNavigateToHome = {
-                // 홈 화면 경로가 정의되면 그곳으로 이동
-                // navController.navigate(Route.Home) {
-                //     popUpTo(Route.Login) { inclusive = true }
-                // }
-                logd("홈 화면으로 이동 (아직 구현되지 않음)")
+                navController.navigate(Route.Main) {
+                    popUpTo(Route.Login) { inclusive = true }
+                }
+                logd("홈 화면으로 이동")
             }
         )
     }
