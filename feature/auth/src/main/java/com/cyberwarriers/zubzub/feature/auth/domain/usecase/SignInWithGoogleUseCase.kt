9259e91d0866ad6c019b107991fa7458950ff046
stopstone <wltjr6432@naver.com>
2025-07-04 13:31:47 +0900

    Feat: 구글 로그인 UseCase 추가
    구글 로그인 UseCase를 추가했습니다.
    AuthRepository를 사용하여 구글 로그인을 수행합니다.

---
+++ feature/auth/src/main/java/com/cyberwarriers/zubzub/feature/auth/domain/usecase/SignInWithGoogleUseCase.kt
@@ -0,0 +1,13 @@
+package com.cyberwarriers.zubzub.feature.auth.domain.usecase
+
+import com.cyberwarriers.zubzub.feature.auth.domain.repository.AuthRepository
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import javax.inject.Inject
+
+class SignInWithGoogleUseCase @Inject constructor(
+    private val authRepository: AuthRepository,
+){
+    suspend operator fun invoke(account: GoogleSignInAccount): Result<Unit> {
+        return authRepository.signWithGoogle(account)
+    }
+}
