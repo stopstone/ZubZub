8e6514fa2332a6a9da269d62ef162c504a2c34d8
StopStone <wltjr6432@naver.com>
2025-07-11 00:41:25 +0900

    Feat: 데이터 모듈 구성
    - 데이터 모듈을 생성하고 의존성을 설정합니다.
    - Firebase 인증 및 Firestore 라이브러리를 추가합니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:util"))
     implementation(project(":core:navigation"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:data"))
     implementation(project(":feature:splash"))
     implementation(project(":feature:auth"))
     implementation(project(":feature:main"))
